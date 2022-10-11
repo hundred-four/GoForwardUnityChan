faf2461d2803c6b6dd48b140c2c3f1818a7e363b
hundred-four <112915629+hundred-four@users.noreply.github.com>
2022-10-11 17:24:04 +0900

    Initial commit

---
+++ Assets/UIController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject gameoverText;
+    private GameObject runLengthText;
+    private float len = 0;
+    private float speed = 5f;
+    private bool isGameover = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameover)
+        {
+            this.len += this.speed * Time.deltaTime;
+            this.runLengthText.GetComponent<Text>().text = "Distance:" + len.ToString("F2") + "m";
+        }
+
+        if (isGameover)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("Samplescene");
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        this.gameoverText.GetComponent<Text>().text = "GAME OVER";
+        this.isGameover = true;
+    }
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityChanController : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody2D rigid2D;
+    private float groundLevel = -3.0f;
+    private float dump = 0.8f;
+    private float jumpVelocity = 20;
+    private float deadLine = -9;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.animator.SetFloat("Horizontal", 1);
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        animator.SetBool("isGround", isGround);
+        GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+
+        if (Input.GetMouseButtonDown(0) && isGround)
+        {
+            this.rigid2D.velocity=new Vector2(0, jumpVelocity);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (this.rigid2D.velocity.y > 0)
+            {
+                this.rigid2D.velocity *= this.dump;
+            }
+        }
+
+        if (transform.position.x < this.deadLine)
+        {
+            GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+            Destroy(gameObject);
+        }
+    }
+}
